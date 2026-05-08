c66d0de6bb7e2cff19f0e4cddc9c0a86aab57418
Dwayne <dwaynepr13@gmail.com>
2026-05-08 20:01:19 +0200

    More editing done to make the website neater and I had to clean up my Booking Create Page as it led to my pages breaing when launching the websites and trying to change between pages.

--- EventEase/Program.cs
+++ EventEase/Program.cs
@@ -1,4 +1,5 @@
 using EventEase.Data;
+using EventEase.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<BlobService>();
 
 var app = builder.Build();
 
---
+++ EventEase/Services/BlobService.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+
+namespace EventEase.Services
+{
+    public class BlobService
+    {
+        private readonly BlobContainerClient _containerClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            string connectionString = configuration["AzureBlobStorage:ConnectionString"];
+
+            string containerName = configuration["AzureBlobStorage:ContainerName"];
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            _containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            _containerClient.CreateIfNotExists();
+        }
+    }
+}
